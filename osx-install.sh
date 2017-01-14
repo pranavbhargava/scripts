e1fe05cc2efb9a86c87e41a6a2f9f305d0b70575
Pranav Bhargava <pranav.bhargava@gmail.com>
2017-01-14 10:37:22 -0800

    Latest Sierra Upgrade

--- osx-install.sh
+++ osx-install.sh
@@ -3,54 +3,107 @@ echo Install all AppStore Apps at first!
 # no solution to automate AppStore installs
 read -p "Press any key to continue... " -n1 -s
 echo  '\n'
- 
-echo Install and Set San Francisco as System Font
-ruby -e "$(curl -fsSL https://raw.github.com/wellsriley/YosemiteSanFranciscoFont/master/install)"
-echo Install Homebrew, wget and cask
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-brew tap homebrew/science
-brew install wget
-brew install node
-brew install ant
-brew install ffmpeg
-brew install vim
-npm install -g gitjk
- 
-brew tap phinze/cask
-brew install brew-cask
- 
-# Core Functionality
-echo Install Core Apps
-brew cask install  dropbox
-brew cask install  iterm2
- 
-## get from App Store
- 
-# Development
-echo Install Dev Apps
-brew cask install github
-brew cask install sublime-text
-brew cask install macvim
-#brew cask install virtualbox
-brew cask install vagrant
-brew cask install sourcetree
-brew cask install charles
- 
-# Google Slavery
-echo Install Google Apps | Chrome not included cause of 1Password Plugin
-# brew cask install google-chrome
-brew cask install google-drive
-brew cask install chromecast
- 
-# Nice to have
-echo Install Some additional Apps
-brew cask install firefox
- 
-brew cask install skype
-brew cask install spotify
-brew cask install spotify-notifications
 
+# Install Xcode Command Line Tools
+xcode-select --install
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+#App store installation
+brew install mas
+
+# Readline
+brew install readline
+
+# cloc
+brew install cloc
+
+# Bash
+brew install bash
+
+# Bash Completion
+brew install bash-completion
+
+# Colorized Cat
+brew install ccat
+
+# CTags
+brew install ctags
+
+# Diff So Fancy
+brew install diff-so-fancy
+
+
+# direnv
+brew install direnv
+
+#wget
+brew install wget
+
+#ant
+brew install ant
+
+#vim
+brew install vim
+
+#nodejs
+brew install node
+
+
+brew tap homebrew/completions
+
+#slack
+brew cask install slack
+
+#google chrome
+brew cask install google-chrome
+
+#dropbox
+brew cask install  dropbox
+
+#firefox
+brew cask install firefox
+
+#sublime-text
+brew cask install sublime-text
+
+#sourcetree
+brew cask install sourcetree
+
+#charles proxy
+brew cask install charles
+
+#skype
+brew cask install skype
+
+#iterm
+brew cask install iterm2
+
+#Github Desktop
+brew cask install github-desktop
+
+#VirtualBox
+brew cask install virtualbox
+
+#Docker
+brew cask install docker
+
+#Spring Tool Suite
+brew cask install sts
+
+#whatsapp
+brew cask install whatsapp
+
+#Ring
+brew cask install ring
+
+#https://github.com/mapmeld/gitjk
+npm install -g gitjk
+
+ 
 #Install ZSH
 brew install zsh
 chsh -s /bin/zsh
